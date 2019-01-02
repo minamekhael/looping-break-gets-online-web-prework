cad592336c1423098e11935c24e9a704a62c3034
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-02 23:39:45 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,12 @@
 
 def levitation_quiz
-  
+  loop do
+  puts "What is the spell that enacts levitation?"
+    answer = get.chomp
+  break
+    answer == "Wingardium Leviosa"
+  end
+  "You passed the quiz!"
 end
 
 
